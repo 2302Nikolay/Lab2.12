9887fddfbee8aade36017e8812e97ab72f97e929
2302Nikolay <Malzewn@gmail.com>
2022-10-16 13:22:57 +0300

    Выполнил индивидуальное заднание

--- Индивидуальное задание/ind_task_var_1.py
+++ Индивидуальное задание/ind_task_var_1.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
+"""""
+Объявите функцию с именем get_sq , которая вычисляет площадь прямоугольника по двум
+параметрам: width и height – ширина и высота прямоугольника и возвращает результат.
+Определите декоратор для этой функции с именем (внешней функции) func_show , который
+отображает результат на экране в виде строки (без кавычек): "Площадь прямоугольника:
+<значение>". Вызовите декорированную функцию get_sq .
+
+"""""
+
+
+def func_show(func):
+    def wrapper(width, height):
+        res = func(width, height)
+        print(f"Площадь прямоугольника: {res}")
+        return res
+
+    return wrapper
+
+
+@func_show
+def get_sq(width, height):
+    return width * height
 
 
 if __name__ == '__main__':
-    print("Hello!")
+    w = int(input("Введите ширину: "))
+    h = int(input("Введите высоту: "))
+    get_sq(w, h)
